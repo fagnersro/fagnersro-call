fbb544e5c41711946a6bf039b3e4862bc4465b7b
fagnersro <fagner.hg40@gmail.com>
2023-05-09 14:53:54 -0300

    User registration - Component: ClaimUsernameForm

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,9 @@
 import { Heading, Text } from '@ignite-ui/react'
 import Image from 'next/image'
 import { Container, Hero, Preview } from './styles'
-
 import previewImage from '../../assets/app-preview.png'
+
+import { ClaimUsernameForm } from './components/ClaimUsernameForm'
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
           Conecte seu calendário e permita que as pessoas marquem agendamentos
           no seu tempo livre.
         </Text>
+        <ClaimUsernameForm />
       </Hero>
 
       <Preview>
