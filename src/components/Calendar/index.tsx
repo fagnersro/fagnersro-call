67c371483bdf32d97f9cb1e3b1341149554f8243
fagnersro <fagner.hg40@gmail.com>
2023-05-13 19:37:51 -0300

    Calendar & Availability - Navigating the Months

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -8,22 +8,43 @@ import {
   CalendarTitle,
 } from './styles'
 import { getWeekDays } from '@/utils/get-week-days'
+import { useState } from 'react'
+import dayjs from 'dayjs'
 
 export function Calendar() {
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs().set('date', 1)
+  })
+
+  function handlePreviousMonth() {
+    const previusMonthDate = currentDate.subtract(1, 'month')
+
+    setCurrentDate(previusMonthDate)
+  }
+
+  function handleNextMonth() {
+    const previusMonthDate = currentDate.add(1, 'month')
+
+    setCurrentDate(previusMonthDate)
+  }
+
+  const currentMonth = currentDate.format('MMMM')
+  const currentYear = currentDate.format('YYYY')
+
   const shortWeekDays = getWeekDays({ short: true })
 
   return (
     <CalendarContainer>
       <CalendarHeader>
         <CalendarTitle>
-          Maio <span>2022</span>
+          {currentMonth} <span>{currentYear}</span>
         </CalendarTitle>
 
         <CalendarActions>
-          <button>
+          <button onClick={handlePreviousMonth} title="Previus month">
             <CaretLeft />
           </button>
-          <button>
+          <button onClick={handleNextMonth} title="Next month">
             <CaretRight />
           </button>
         </CalendarActions>
