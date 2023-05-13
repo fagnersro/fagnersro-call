9639f3484d8451235dcd2139f1698c4ead24f6f0
fagnersro <fagner.hg40@gmail.com>
2023-05-13 17:37:40 -0300

    Calendar and availability - Visual structure of the timetable

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,10 +1,38 @@
 import { Calendar } from '@/components/Calendar'
-import { Container } from './styles'
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerList,
+  TimerPickerItem,
+} from './styles'
 
 export function CalendarStep() {
+  const isDateSelected = true
+
   return (
-    <Container>
+    <Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            terça-feira <span>20 de setembro</span>
+          </TimePickerHeader>
+          <TimePickerList>
+            <TimerPickerItem>08:00h</TimerPickerItem>
+            <TimerPickerItem>09:00h</TimerPickerItem>
+            <TimerPickerItem>10:00h</TimerPickerItem>
+            <TimerPickerItem>11:00h</TimerPickerItem>
+            <TimerPickerItem>12:00h</TimerPickerItem>
+            <TimerPickerItem>13:00h</TimerPickerItem>
+            <TimerPickerItem>14:00h</TimerPickerItem>
+            <TimerPickerItem>15:00h</TimerPickerItem>
+            <TimerPickerItem>16:00h</TimerPickerItem>
+            <TimerPickerItem>17:00h</TimerPickerItem>
+            <TimerPickerItem>18:00h</TimerPickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Container>
   )
 }
