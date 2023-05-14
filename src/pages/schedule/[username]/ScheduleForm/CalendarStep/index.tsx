47631fb8ab1b7e45ddc40c127355b3af4c6f6e03
fagnersro <fagner.hg40@gmail.com>
2023-05-13 23:02:57 -0300

    Calendar and Availability - Preparing calendar

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -6,13 +6,15 @@ import {
   TimePickerList,
   TimerPickerItem,
 } from './styles'
+import { useState } from 'react'
 
 export function CalendarStep() {
-  const isDateSelected = false
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+  const isDateSelected = !!selectedDate
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
-      <Calendar />
+      <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
       {isDateSelected && (
         <TimePicker>
           <TimePickerHeader>
