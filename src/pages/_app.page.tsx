5c3e5c6329b37467b82fa79b628f6da5af39260b
fagnersro <fagner.hg40@gmail.com>
2023-05-14 16:40:41 -0300

    Calendar and Availability - Configuring React Query

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -3,6 +3,8 @@ import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { globalStyles } from '@/styles/global'
 import { Roboto } from 'next/font/google'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '@/lib/react-query'
 
 const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] })
 globalStyles()
@@ -12,10 +14,12 @@ export default function App({
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <main className={roboto.className}>
-        <Component {...pageProps} />
-      </main>
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <main className={roboto.className}>
+          <Component {...pageProps} />
+        </main>
+      </SessionProvider>
+    </QueryClientProvider>
   )
 }
